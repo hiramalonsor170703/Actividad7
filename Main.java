28afaa03f22fbce58171aa3b9920d2764deb0ba3
hiramalonsor170703 <141661262+hiramalonsor170703@users.noreply.github.com>
2023-10-31 10:14:58 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package ListFibonacci;
 public class Main {
 	public static void main (String[]args) {
 		
-		Ejecutar deuna = new Ejecutar();
-		deuna.ejecutar();
+		Manager manager = new Manager();
+		manager.ejecutar();
 	  }
 	}
